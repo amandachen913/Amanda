66454abb4f70b727b3b292232f39dea913bfaf2d
amandachen913 <126211491+amandachen913@users.noreply.github.com>
2023-03-09 08:10:52 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,70 @@
-basic.forever(function () {
-	
+let 骰子 = 0
+input.onGesture(Gesture.Shake, function () {
+    骰子 = randint(1, 6)
+    if (骰子 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+    if (骰子 == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+    	
+    }
+    if (骰子 == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+    	
+    }
+    if (骰子 == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+    	
+    }
+    if (骰子 == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+    	
+    }
+    if (骰子 == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+    	
+    }
 })
